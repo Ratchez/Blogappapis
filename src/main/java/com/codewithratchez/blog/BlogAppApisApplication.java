620cc576d27c931e3a628b4326eec7cbf4a7709c
Rasugu <KEN22926@kcbgroup.com>
2023-05-09 12:40:21 +0300

    assign role for specific users

--- src/main/java/com/codewithratchez/blog/BlogAppApisApplication.java
+++ src/main/java/com/codewithratchez/blog/BlogAppApisApplication.java
@@ -1,5 +1,8 @@
 package com.codewithratchez.blog;
 
+import com.codewithratchez.blog.config.AppConstants;
+import com.codewithratchez.blog.entities.Role;
+import com.codewithratchez.blog.repositories.RoleRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -8,10 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 @SpringBootApplication
 public class BlogAppApisApplication implements CommandLineRunner{
 	@Autowired
 	private PasswordEncoder passwordEncoder;
+	@Autowired
+	private RoleRepo roleRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
@@ -25,5 +32,24 @@ public class BlogAppApisApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println(passwordEncoder.encode("xyz"));
+		try {
+			Role role = new Role();
+			role.setRoleId(AppConstants.ADMIN_USER);
+			role.setName("ADMIN_USER");
+
+			Role role1 = new Role();
+			role1.setRoleId(AppConstants.NORMAL_USER);
+			role1.setName("NORMAL_USER");
+
+			List<Role> roles =  List.of(role, role1);
+			List<Role> result = roleRepo.saveAll(roles);
+
+			result.forEach(r ->{
+				System.out.println(r.getName());
+			});
+
+		}catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 }
