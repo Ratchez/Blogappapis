f49a5d4e0c952f576793d033211319557c99e6a0
Rasugu <KEN22926@kcbgroup.com>
2023-05-09 12:50:24 +0300

    add register user method

--- src/main/java/com/codewithratchez/blog/services/UserService.java
+++ src/main/java/com/codewithratchez/blog/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
+    UserDto registerNewUser(UserDto user);
     UserDto createUser(UserDto user);
     UserDto updateUser(UserDto user, Integer userId);
     UserDto getUserByStudentId(String userId);
