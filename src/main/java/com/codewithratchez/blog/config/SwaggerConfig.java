4ebf748346ceaf33bba31f946f2dbe5aa2526720
Rasugu <KEN22926@kcbgroup.com>
2023-05-10 16:45:45 +0300

    create swaggerconfig file

--- src/main/java/com/codewithratchez/blog/config/SwaggerConfig.java
+++ src/main/java/com/codewithratchez/blog/config/SwaggerConfig.java
@@ -1,2 +1,20 @@
-package com.codewithratchez.blog.config;public class SwaggerConfig {
+package com.codewithratchez.blog.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI baseOpenApi(){
+        return new OpenAPI().info(new Info().title("Blog app : Backend").description("Project that has functionalities of blog").version("1.0")
+                .termsOfService("Terms of Service").contact(new Contact().name("Ratchez").email("royrasugu38@gmail.com").url("https://royrasugu.github.io/Portfolio/"))
+                .license(new License().name("license of APIs")));
+    }
 }
