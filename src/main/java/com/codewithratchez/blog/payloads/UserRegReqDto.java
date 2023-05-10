46e468ac74f345f74e2a99a5799c8c3fead41246
Rasugu <KEN22926@kcbgroup.com>
2023-05-10 11:22:54 +0300

    introduce new registration payload

---
+++ src/main/java/com/codewithratchez/blog/payloads/UserRegReqDto.java
@@ -0,0 +1,36 @@
+package com.codewithratchez.blog.payloads;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class UserRegDto {
+    private int id;
+    @NotEmpty
+    @NotBlank
+    @Size(min = 4, message = "Username must be have min of 4 characters")
+    private String name;
+    @Email(message = "Email address is not valid")
+    @NotBlank
+    private String email;
+
+    @NotEmpty
+    @NotBlank
+    @Size(min = 3, max = 10, message = "Password must be min of 3 chars and max of 10 chars")
+    private String password;
+    @NotEmpty
+    private String about;
+
+    private String studentId;
+
+//    private Set<RoleDto> roles = new HashSet<>();
+}
