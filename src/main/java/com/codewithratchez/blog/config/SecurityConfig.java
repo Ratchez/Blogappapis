27388d5b8e7da0725fbe65cbbfbf1846c5bdf76f
Rasugu <KEN22926@kcbgroup.com>
2023-05-10 11:27:40 +0300

    ident code

--- src/main/java/com/codewithratchez/blog/config/SecurityConfig.java
+++ src/main/java/com/codewithratchez/blog/config/SecurityConfig.java
@@ -21,9 +21,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebSecurity
+@EnableWebMvc
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     @Autowired
@@ -40,6 +42,7 @@ public class SecurityConfig {
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers("/api/v1/auth/**").permitAll()
+                .requestMatchers("/v3/api-docs").permitAll()
                 .requestMatchers(HttpMethod.GET).permitAll()
                 .anyRequest()
                 .authenticated()
