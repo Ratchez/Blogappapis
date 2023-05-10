d896c742d57240b5b98b689db43583cef58b3e25
Rasugu <KEN22926@kcbgroup.com>
2023-05-10 11:39:19 +0300

    use userregreq in registering a new user

--- src/main/java/com/codewithratchez/blog/services/UserService.java
+++ src/main/java/com/codewithratchez/blog/services/UserService.java
@@ -1,12 +1,14 @@
 package com.codewithratchez.blog.services;
 
 import com.codewithratchez.blog.payloads.UserDto;
+import com.codewithratchez.blog.payloads.UserRegReqDto;
+import com.codewithratchez.blog.payloads.UserRegRespDto;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface UserService {
-    UserDto registerNewUser(UserDto user);
+    UserRegRespDto registerNewUser(UserRegReqDto user);
     UserDto createUser(UserDto user);
     UserDto updateUser(UserDto user, Integer userId);
     UserDto getUserByStudentId(String userId);
