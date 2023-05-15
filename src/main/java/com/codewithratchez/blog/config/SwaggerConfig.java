8c87fdbad7d25abd2f95b3a53af3dbb2adcd94b7
Rasugu <KEN22926@kcbgroup.com>
2023-05-15 14:37:34 +0300

    add security requirement annotation

--- src/main/java/com/codewithratchez/blog/config/SwaggerConfig.java
+++ src/main/java/com/codewithratchez/blog/config/SwaggerConfig.java
@@ -1,15 +1,19 @@
 package com.codewithratchez.blog.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition
+@OpenAPIDefinition(security = {@SecurityRequirement(name = "bearer-key")})
 public class SwaggerConfig {
     @Bean
     public OpenAPI baseOpenApi(){
@@ -17,4 +21,10 @@ public class SwaggerConfig {
                 .termsOfService("Terms of Service").contact(new Contact().name("Ratchez").email("royrasugu38@gmail.com").url("https://royrasugu.github.io/Portfolio/"))
                 .license(new License().name("license of APIs")));
     }
+//    @Bean
+//    public OpenApiCustomiser customerGlobalHeaderOpenApiCustomiser() {
+//        return openApi -> openApi.getComponents()
+//                .addSecuritySchemes("bearer-key",
+//                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
+//    }
 }
