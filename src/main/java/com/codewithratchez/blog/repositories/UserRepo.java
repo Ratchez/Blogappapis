d395c8f3ca8c045f62fc8640ffb29139738e00bd
Rasugu <KEN22926@kcbgroup.com>
2023-05-19 10:33:15 +0300

    add optional method of find user by bloggerId

--- src/main/java/com/codewithratchez/blog/repositories/UserRepo.java
+++ src/main/java/com/codewithratchez/blog/repositories/UserRepo.java
@@ -4,11 +4,9 @@ import com.codewithratchez.blog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
-import java.util.UUID;
 
 
 public interface UserRepo extends JpaRepository<User, Integer> {
-
-//    Optional<User> findByStudentId(String userId);
+    Optional<User> findByBloggerId(String bloggerId);
     Optional<User> findByEmail(String email);
 }
