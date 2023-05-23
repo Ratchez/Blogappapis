61d73d1b7d512aa0c7b88fd9d3607e3dad45aafc
Rasugu <KEN22926@kcbgroup.com>
2023-05-23 14:59:33 +0300

    change value of user roles

--- src/main/java/com/codewithratchez/blog/config/AppConstants.java
+++ src/main/java/com/codewithratchez/blog/config/AppConstants.java
@@ -5,6 +5,7 @@ public class AppConstants {
     public static final String PAGE_SIZE = "10";
     public static final String SORT_BY = "postId";
     public static final String SORT_DIR = "asc";
-    public static final Integer ROLE_USER = 502;
-    public static final Integer ROLE_ADMIN = 501;
+    public static final Integer ROLE_USER = 3;
+    public static final Integer ROLE_ADMIN = 1;
+    public static final Integer ROLE_EDITOR = 2;
 }
