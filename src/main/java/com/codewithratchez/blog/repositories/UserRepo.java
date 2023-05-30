d8eb13fdef41c42178f71fca5397ab4e55bee390
Rasugu <KEN22926@kcbgroup.com>
2023-05-30 14:18:12 +0300

    add new method of check user exists by email

--- src/main/java/com/codewithratchez/blog/repositories/UserRepo.java
+++ src/main/java/com/codewithratchez/blog/repositories/UserRepo.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface UserRepo extends JpaRepository<User, Integer> {
     Optional<User> findByBloggerId(Integer bloggerId);
     Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
 }
